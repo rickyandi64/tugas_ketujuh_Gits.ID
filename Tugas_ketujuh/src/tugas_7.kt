3eb61ae8771d54f15a7625ee6770c998d2409713
Rickyandikurniawan64 <39975146+rickyandi64@users.noreply.github.com>
2021-09-09 15:57:21 +0700

    Update tugas_7.kt

--- Tugas_ketujuh/src/tugas_7.kt
+++ Tugas_ketujuh/src/tugas_7.kt
@@ -1,11 +1,13 @@
-class Data_Mahasiswa(_nama_mahasiswa: String, _Jurusan: String = "Sistem Informasi"){
+class Data_Mahasiswa(_nama_mahasiswa: String, _Jurusan: String = "Sistem Informasi", _Universitas: String = "Universitas Internasional Batam"){
     val nama_mahasiswa = _nama_mahasiswa
     var Jurusan = _Jurusan
+    var Universitas = _Universitas
 
 
     fun Hasil(): String{
         val hasil = "--- Data Mahasiswa ---\nNama Mahasiswa: " + nama_mahasiswa +
-                "\nJurusan: " + Jurusan
+                "\nJurusan: " + Jurusan +
+                "\n Dari Universitas: " + Universitas
         return hasil
     }
 }
